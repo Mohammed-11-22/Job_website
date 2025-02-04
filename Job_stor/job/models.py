58b9d4fac8055d7679676f43f639d985d68de194
Mohammed-11-22 <mrmedmohammed21@gmail.com>
2025-02-05 00:21:41 +0100

    job models edit

--- Job_stor/job/models.py
+++ Job_stor/job/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+JOB_TYPE = (
+    ('full time','full time'),
+    ('part time','part time'),
+)
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    #location
+    job_type = models.CharField(max_length=30 , choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateField(auto_now=True)
